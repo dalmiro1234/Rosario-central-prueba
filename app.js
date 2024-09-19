b75caeee29e801b314ff41d65d6c07117b8b7b53
Dalmiro Sander <dalmirosander@gmail.com>
2024-09-19 14:51:03 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let mi_nimbre  = "Dalmiro";
+let numeroA = prompt("Ingresa tu primer numero");
+let numeroB = prompt("Ingresa tu segundo numero");
+
+let resultado = numeroA + numeroB;
+
+
+console.log(parseInt(resultado)); 
+// Mensaje de bienvenida desde el archivo JavaScript
+console.log('Hola Mundo desde el archivo JavaScript!');
